5388afc0ad07882fcdd445376fa74507d2874df4
Giulio Giovanni Altomari <giuliogiovanni.altomari@mail.polimi.it>
2024-04-29 16:23:44 +0200

    trying to speed up things

---
+++ src/main/java/Client/GUI/ClientGUI.java
@@ -0,0 +1,14 @@
+package Client.GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientGUI extends Application {
+    public static void main(String[] args) {
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
