ebd6736300ec87f017c1009cc2214c29559e4093
Giulio Giovanni Altomari <giuliogiovanni.altomari@mail.polimi.it>
2024-05-05 15:27:06 +0200

    quick pause for lunch, starting cli

--- src/main/java/Model/Board/PersonalBoard/PersonalBoard.java
+++ src/main/java/Model/Board/PersonalBoard/PersonalBoard.java
@@ -1,4 +1,23 @@
 package Model.Board.PersonalBoard;
 
+import Model.Cards.Card;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PersonalBoard {
+    private Map<Integer, Map<Integer, Card>> boardMap;
+
+    public PersonalBoard() {
+        boardMap = new HashMap<>();
+        int id = 0;
+    }
+
+    public void placeCard(int x, int y, Card card) {
+        boardMap.computeIfAbsent(x, k -> new HashMap<>()).put(y, card);
+    }
+
+    public Card getCard(int x, int y) {
+        return boardMap.getOrDefault(x, new HashMap<>()).get(y);
+    }
 }
