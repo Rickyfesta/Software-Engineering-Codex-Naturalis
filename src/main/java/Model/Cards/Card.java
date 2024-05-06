96971c1e723ec478d3ea0a6edbbda467013e9612
Giulio Giovanni Altomari <giuliogiovanni.altomari@mail.polimi.it>
2024-05-06 23:18:26 +0200

    renewing the card class

--- src/main/java/Model/Cards/Card.java
+++ src/main/java/Model/Cards/Card.java
@@ -2,7 +2,7 @@ package Model.Cards;
 
 
 public class Card {
-    private boolean top, left, right, bottom;  //4 boolean for the corners
+    private String top, left, right, bottom;  //4 boolean for the corners
 
     protected String type; // "Risorsa" o "Oro"
     protected String mainResource; // "Plants", "Animal", "Mushroom", "Insect"
@@ -14,17 +14,14 @@ public class Card {
         this.type = type;
         this.points = points;
 
-        this.top = top;
-        this.left = left;
-        this.right = right;
-        this.bottom = bottom;
+        this.top = String.valueOf(top);
+        this.left = String.valueOf(left);
+        this.right = String.valueOf(right);
+        this.bottom = String.valueOf(bottom);
 
     }
     //If corner playable then allows to be covered by other cards
-    public boolean isTopPlayable() { return top;}
-    public boolean isLeftPlayable() { return left; }
-    public boolean isRightPlayable() { return right; }
-    public boolean isBottomPlayable() { return bottom; }
+
 
     public int getPoints (){
         return this.points;
