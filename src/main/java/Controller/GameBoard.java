a01021e87de7f1e524b5062ede26329ddf74f6ca
mtonydavid <tonydavid.matrundola@mail.polimi.it>
2024-05-09 13:00:26 +0200

    main board and common board , fixed some scenebuilder

--- src/main/java/Controller/GameBoard.java
+++ src/main/java/Controller/GameBoard.java
@@ -1,10 +1,43 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class GameBoard {
     @FXML
-    private Pane cardPane;
+    private Pane mainBoardPane;
+    @FXML
+    private HBox handCardHBox;
+    @FXML
+    private Button commonBoardButton;
 
+    private Stage commonBoardStage;
+
+    private void initialize() {
+        setupCommonBoard();
+    }
+    private void setupCommonBoard() {
+        try {
+            FXMLLoader loader= new FXMLLoader(getClass().getResource("/GUI/CommonBoard.fxml"));
+            Pane commonBoard = loader.load();
+            commonBoardStage = new Stage();
+            commonBoardStage.setScene(new Scene(commonBoard));
+            commonBoardStage.setTitle("Common Board");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void toggleCommonBoard() {
+        if (commonBoardStage.isShowing()) {
+            commonBoardStage.hide();
+        } else {
+            commonBoardStage.show();
+        }
+    }
 }
