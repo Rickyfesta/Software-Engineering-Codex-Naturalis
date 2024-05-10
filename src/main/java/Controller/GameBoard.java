13bf0bc254b0aebb1868d21171b56198a6ca072c
Giulio Giovanni Altomari <giuliogiovanni.altomari@mail.polimi.it>
2024-05-10 09:44:20 +0200

    gui making, hand, decks, starting card, objectives

--- src/main/java/Controller/GameBoard.java
+++ src/main/java/Controller/GameBoard.java
@@ -2,15 +2,11 @@ package Controller;
 
 import Model.Cards.RandomCardFile;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,11 +21,6 @@ public class GameBoard {
     @FXML
     private HBox handCardsHBox;
     @FXML
-    private Button commonBoardButton;
-    @FXML
-    private StackPane commonBoardContainer;
-
-    @FXML
     private ImageView CardHand1;
 
     @FXML
@@ -37,25 +28,43 @@ public class GameBoard {
 
     @FXML
     private ImageView CardHand3;
+    @FXML
+    private ImageView CommonObj;
+
+    @FXML
+    private ImageView GoldDeck;
+
+    @FXML
+    private ImageView PersonalObj;
+
+    @FXML
+    private ImageView ResDeck;
+
+    @FXML
+    private ImageView StartingCard;
 
     @FXML
     void initialize() {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/CommonBoard.fxml"));
-        Pane commonBoard = null;
-        try {
-            commonBoard = loader.load();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         // Set CommonBoard into the placeholder
-        commonBoardContainer.getChildren().add(commonBoard);
-        //System.out.println("/Cards/cardsimg/" + RandomCardFile.getRandomGXXFileName());
+        System.out.println("/Cards/cardsimg/" + RandomCardFile.getRandomGXXFileName());
         CardHand1.setImage(new Image("cards/carding/" + RandomCardFile.getRandomGXXFileName()));
         CardHand2.setImage(new Image("cards/carding/" + RandomCardFile.getRandomXXFileName()));
         CardHand3.setImage(new Image("cards/carding/" + RandomCardFile.getRandomXXFileName()));
+        CommonObj.setImage(new Image("cards/carding/" + RandomCardFile.getRandomOXXFileName()));
+        GoldDeck.setImage(new Image("cards/carding/" + RandomCardFile.getRandomGXXFileName()));
+        PersonalObj.setImage(new Image("cards/carding/" + RandomCardFile.getRandomOXXFileName()));
+        ResDeck.setImage(new Image("cards/carding/" + RandomCardFile.getRandomXXFileName()));
+        StartingCard.setImage(new Image("cards/carding/" + RandomCardFile.getRandomSXXFileName()));
+
         //System.out.println(RandomCardFile.getRandomXXFileName());
-        assert commonBoardButton != null : "fx:id=\"commonBoardButton\" was not injected: check your FXML file 'GameBoard.fxml'.";
-        assert handCardsHBox != null : "fx:id=\"handCardsHBox\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert CardHand1 != null : "fx:id=\"CardHand1\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert CardHand2 != null : "fx:id=\"CardHand2\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert CardHand3 != null : "fx:id=\"CardHand3\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert CommonObj != null : "fx:id=\"CommonObj\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert GoldDeck != null : "fx:id=\"GoldDeck\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert PersonalObj != null : "fx:id=\"PersonalObj\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert ResDeck != null : "fx:id=\"ResDeck\" was not injected: check your FXML file 'GameBoard.fxml'.";
+        assert StartingCard != null : "fx:id=\"StartingCard\" was not injected: check your FXML file 'GameBoard.fxml'.";
         assert mainBoardPane != null : "fx:id=\"mainBoardPane\" was not injected: check your FXML file 'GameBoard.fxml'.";
 
     }
