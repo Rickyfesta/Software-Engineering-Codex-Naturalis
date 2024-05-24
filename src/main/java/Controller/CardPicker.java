aebde141958c8eccd2c44880554f46cedf516f31
Giulio Giovanni Altomari <giuliogiovanni.altomari@mail.polimi.it>
2024-05-24 22:57:04 +0200

    cards now can be picked up from decks

---
+++ src/main/java/Controller/CardPicker.java
@@ -0,0 +1,69 @@
+package Controller;
+
+import Model.Cards.RandomCardFile;
+import javafx.animation.ScaleTransition;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.util.List;
+
+public class CardPicker {
+
+    static List<ImageView> StartDecksList;
+    public CardPicker(GameBoard gameBoard) {
+    }
+
+    public static void PickNewCard(ImageView handCard) {
+        //Here i make pickable the cards of the decks.
+        for (ImageView imageView : StartDecksList) {
+            imageView.setOnMouseClicked(event -> {
+                // Handle the mouse click event here
+                String fileName = extractFileName(imageView.getImage().getUrl());
+                //System.out.println(fileName);
+                handCard.setImage(new Image("/" + fileName));
+                if(fileName.startsWith("G")){
+                    imageView.setScaleX(0.1);
+                    imageView.setScaleY(0.1);
+                    ScaleTransition initialTransition = new ScaleTransition(Duration.millis(100), imageView);
+                    initialTransition.setToX(1.0);
+                    initialTransition.setToY(1.0);
+                    initialTransition.play();
+                    imageView.setImage(new Image("/" + RandomCardFile.getRandomGXXFileName()));
+                }else{
+                    imageView.setScaleX(0.1);
+                    imageView.setScaleY(0.1);
+                    ScaleTransition initialTransition = new ScaleTransition(Duration.millis(100), imageView);
+                    initialTransition.setToX(1.0);
+                    initialTransition.setToY(1.0);
+                    initialTransition.play();
+                    imageView.setImage(new Image("/" + RandomCardFile.getRandomXXFileName()));
+                }
+            });
+            }
+        }
+
+    public void makePickable(ImageView CardToPick, List<ImageView> decksList) {
+        StartDecksList = decksList;
+        // Create scale transition for mouse hover
+        ScaleTransition hoverTransition = new ScaleTransition(Duration.millis(50), CardToPick);
+        hoverTransition.setToX(1.1);
+        hoverTransition.setToY(1.1);
+        // Create scale transition for mouse exit
+        ScaleTransition exitTransition = new ScaleTransition(Duration.millis(50), CardToPick);
+        exitTransition.setToX(1.0);
+        exitTransition.setToY(1.0);
+        CardToPick.setOnMouseEntered(event -> hoverTransition.playFromStart());
+        CardToPick.setOnMouseExited(event -> exitTransition.playFromStart());
+    }
+
+    private static String extractFileName(String filePath) {
+        if (filePath != null && !filePath.isEmpty()) {
+            int lastSlashIndex = filePath.lastIndexOf('/');
+            if (lastSlashIndex != -1 && lastSlashIndex < filePath.length() - 1) {
+                return filePath.substring(lastSlashIndex + 1);
+            }
+        }
+        return "";
+    }
+}
