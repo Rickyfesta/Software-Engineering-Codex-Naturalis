e52cefcc6a1e75415bdc4466f2c379eb082b8fc7
mtonydavid <tonydavid.matrundola@mail.polimi.it>
2024-05-30 10:41:25 +0200

    startscene

---
+++ src/main/java/Controller/PersonalGoalScene.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class PersonalGoalScene {
+    @FXML
+    private ImageView personalGoal1;
+
+    @FXML
+    private ImageView personalGoal2;
+}
---
+++ src/main/java/Controller/StarterCardScene.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+
+public class StarterCardScene {
+    @FXML
+    private ImageView starterCardBack;
+
+    @FXML
+    private ImageView starterCardFront;
+
+}
